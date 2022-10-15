202ecbf9cd76468bf5890dfa38282e3cab53c671
Denis <denis@gmail.com>
2022-10-15 23:09:54 +0300

    Убрал мерацние шрифтов

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -18,18 +18,7 @@ class MyDocument extends Document {
   render(): JSX.Element {
     return (
       <Html lang="ru">
-        <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin="true"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
-            rel="stylesheet"
-          />
-        </Head>
+        <Head></Head>
         <body>
           <Main></Main>
           <NextScript></NextScript>
