e28915f5c4bb372a8d01026f3ee5fa1690bb0a59
Denis <denis@gmail.com>
2022-10-18 19:55:31 +0600

    добавил github на главную

--- src/page-components/AboutPage/aboutPage.tsx
+++ src/page-components/AboutPage/aboutPage.tsx
@@ -1,6 +1,7 @@
 import { AboutObjects } from "../../components/AboutObjects/AboutObjects";
 import { VarText } from "../../components/VarText/VarText";
 import { aboutPageProps } from "./aboutPage.props";
+import styles from "./aboutPage.module.css";
 
 export function AboutPage({ ...props }: aboutPageProps): JSX.Element {
   return (
@@ -8,6 +9,13 @@ export function AboutPage({ ...props }: aboutPageProps): JSX.Element {
       <VarText varname={"myName"} text={"Денис"}></VarText>
       <VarText varname={"mySurname"} text={"Штиллер"}></VarText>
       <VarText varname={"university"} text={"SPBGASU"}></VarText>
+      <VarText
+        varname={"myGitHub"}
+        text={
+          <a target={"_blank"} href="https://github.com/Barbagidon"><span className={styles.gitlink}>github.com/Barbagidon</span>
+          </a>
+        }
+      ></VarText>
       <AboutObjects />
     </div>
   );
