db69dd9c886337272caea3f1cfcc8c39ac7072b5
Denis <denis@gmail.com>
2022-10-21 18:23:09 +0600

    изменил const на let

--- src/components/VarText/VarText.tsx
+++ src/components/VarText/VarText.tsx
@@ -10,14 +10,16 @@ export const VarText = ({
   className,
   ...props
 }: VarTextProps): JSX.Element => {
+  
   return (
     <div {...props} className={cn(styles.textInner, className)}>
       <Htag className={styles.header} tag="h3">
-        <span className={styles.variable}>const</span>
+        <span className={styles.variable}>let</span>
         <span className={styles.varname}>{varname}</span>
         <span className={styles.equals}>=</span>
         {text ? <span className={styles.text}>{text}</span> : null}
       </Htag>
     </div>
   );
+
 };
